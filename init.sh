72df7b78aa0afb02aa157c94ccf36afc8c41b85e
fisantos <filipe.santos@nokia.com>
2025-11-04 19:17:04 +0000

    feat: Removed F13 key from PT keyboard

--- init.sh
+++ init.sh
@@ -3,7 +3,6 @@
 cp -r .i3status/ ~/.config/bin/
 chmod +x workspace-app-launcher.sh
 chmod +x toggle-layout.sh
-sudo cp ~/.config/i3/layouts/xkb/pt /usr/share/X11/xkb/symbols/pt
 sudo cp ~/.config/i3/layouts/xkb/us /usr/share/X11/xkb/symbols/us
 sudo cp ~/.config/i3/layouts/xkb/evdev.lst /usr/share/X11/xkb/rules/evdev.lst
 sudo cp ~/.config/i3/layouts/xkb/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
