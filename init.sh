2d1ed44859a1ddbcb0aa6129537bd23f6d167826
fisantos <filipe.santos@nokia.com>
2025-11-04 18:15:29 +0000

    feat: Adjusted keyboards

--- init.sh
+++ init.sh
@@ -3,5 +3,9 @@
 cp -r .i3status/ ~/.config/bin/
 chmod +x workspace-app-launcher.sh
 chmod +x toggle-layout.sh
+sudo cp ~/.config/i3/layouts/xkb/pt /usr/share/X11/xkb/symbols/pt
+sudo cp ~/.config/i3/layouts/xkb/us /usr/share/X11/xkb/symbols/us
+sudo cp ~/.config/i3/layouts/xkb/evdev.lst /usr/share/X11/xkb/rules/evdev.lst
+sudo cp ~/.config/i3/layouts/xkb/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
 i3-msg reload
 i3-msg restart
--- toggle-layout.sh
+++ toggle-layout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 current=$(setxkbmap -query | grep layout | awk '{print $2}' | cut -d',' -f1)
 if [[ $current == "us" ]]; then
-    setxkbmap pt
+    setxkbmap pt super_l_f14
     echo "PT" > /tmp/current_layout
 else
     setxkbmap us swapped
