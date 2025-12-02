39812741f7f7c1ca825465db820a40ec22a6cf63
fisantos <filipe.santos@nokia.com>
2025-12-02 08:58:19 +0000

    feat: wallpaper

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-CONNECTED_DP_OUTPUT=$(xrandr | grep " connected" | grep -E "DP-[0-9]+-1" | awk '{print $1}' | head -n 1)
+CONNECTED_DP_OUTPUT=$(xrandr | grep " connected" | grep -E "DP-[0-9]+-[0-9]" | awk '{print $1}' | head -n 1)
 
 if [ -z "$CONNECTED_DP_OUTPUT" ]; then
     echo "No connected DP-x-1 output found. Setting eDP-1 as primary."
     xrandr --output eDP-1 --primary --auto
+    ./set-wallpaper.sh
 else
     echo "Found connected DP output: $CONNECTED_DP_OUTPUT. Setting as primary."
     xrandr --output "$CONNECTED_DP_OUTPUT" --primary --auto --left-of eDP-1 --auto
+    ./set-wallpaper.sh
 fi
