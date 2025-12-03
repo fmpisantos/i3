18a54807a1afbb8ca272ddd5e78c5c5de73e0950
fisantos <filipe.santos@nokia.com>
2025-12-03 15:26:13 +0000

    fix wallpaper setting path error on monitor set script

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -4,9 +4,9 @@ CONNECTED_DP_OUTPUT=$(xrandr | grep " connected" | grep -E "DP-[0-9]+-[0-9]" | a
 if [ -z "$CONNECTED_DP_OUTPUT" ]; then
     echo "No connected DP-x-1 output found. Setting eDP-1 as primary."
     xrandr --output eDP-1 --primary --auto
-    ./set-wallpaper.sh
+    ~/.config/i3/set-wallpaper.sh
 else
     echo "Found connected DP output: $CONNECTED_DP_OUTPUT. Setting as primary."
     xrandr --output "$CONNECTED_DP_OUTPUT" --primary --auto --left-of eDP-1 --auto
-    ./set-wallpaper.sh
+    ~/.config/i3/set-wallpaper.sh
 fi
